834785a5cc57c40b1119f3361e0c0ea3e506ffa4
romulogdonadoni <romulogdonadoni@gmail.com>
2023-09-10 17:27:57 -0300

    0.5

--- src/routes/getArtist.js
+++ src/routes/getArtist.js
@@ -7,7 +7,20 @@ router.get("/get/artist", async (req, res) => {
   try {
     const resArtist = await prisma.user.findMany({
       where: { role: "ARTIST" },
-      select: { password: false, username: true, image: true, role: true, album: { include: { music: true } } },
+      select: { id: true, password: false, username: true, image: true, role: true, album: { include: { music: true } } },
+    });
+    res.status(200).json(resArtist);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ message: "algo deu errado!" });
+  }
+});
+router.get("/get/artist/:id", async (req, res) => {
+  const id = req.params["id"];
+  try {
+    const resArtist = await prisma.user.findUnique({
+      where: { id: id },
+      select: { id: true, password: false, username: true, image: true, role: true, album: { include: { music: true } } },
     });
     res.status(200).json(resArtist);
   } catch (error) {
