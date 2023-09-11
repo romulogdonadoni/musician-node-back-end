d9b4423b12b8308a429571aac49581f1cbe2d3ea
romulogdonadoni <romulogdonadoni@gmail.com>
2023-09-11 13:06:22 -0300

    1.0

--- src/routes/getPlayList.js
+++ src/routes/getPlayList.js
@@ -6,15 +6,15 @@ import jwt from "jsonwebtoken";
 import authToken from "./authToken.js";
 
 router.get("/get/playlist", authToken, async (req, res) => {
-  const token = req.headers["authorization"]?.split(" ")[1];
+  const token = req.headers.authorization.split(" ")[1];
   const { id: authorId } = jwt.decode(token);
 
   try {
     const resPlayList = await prisma.playlist.findMany({ where: { authorId: authorId } });
-    res.status(200).json({resPlayList, msg: token, id: authorId });
+    res.status(200).json(resPlayList);
   } catch (error) {
     console.log(error);
-    res.status(400).json({ message: "algo deu errado!", error: error, msg: token, id: authorId });
+    res.status(400).json({ message: "algo deu errado!"});
   }
 });
 
